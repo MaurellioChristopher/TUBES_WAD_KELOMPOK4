923276a8803b4faf9e7e37c45197389a0e4d3a5a
Alya Salma Khoerunnisaa <alyasalmakh8@gmail.com>
2026-01-04 00:27:15 +0700

    Tambahkan timestamp pada relasi skill di model Post

--- App/Models/Post.php
+++ App/Models/Post.php
@@ -35,6 +35,7 @@ class Post extends Model
 
     public function skills()
     {
-        return $this->belongsToMany(Skill::class);
+        return $this->belongsToMany(Skill::class)
+                    ->withTimestamps();
     }
 }
