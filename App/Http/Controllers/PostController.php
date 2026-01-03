65e583d5dcbb3829c4d37b90189274c3c9823c93
Alya Salma Khoerunnisaa <alyasalmakh8@gmail.com>
2026-01-04 01:36:01 +0700

    Menambah return type hints

--- App/Http/Controllers/PostController.php
+++ App/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\RedirectResponse;
+
 
 class PostController extends Controller
 {
