84289e866985690e71d87c94e247e210aa9690f5
JAZZKORD <jazzkord0203@gmail.com>
2026-01-04 11:37:36 +0700

    memberikan pesan eror

--- resources/views/admin/topics/edit.blade.php
+++ resources/views/admin/topics/edit.blade.php
@@ -31,7 +31,7 @@
                         <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"/>
                     </svg>
                     <div class="flex-1">
-                        <h3 class="text-red-800 font-semibold text-sm">Please fix the following errors:</h3>
+                        <h3 class="text-red-800 font-semibold text-sm">Please review and correct the following errors:</h3>
                         <ul class="list-disc list-inside text-red-700 text-sm mt-2 space-y-1">
                             @foreach($errors->all() as $error)
                                 <li>{{ $error }}</li>
