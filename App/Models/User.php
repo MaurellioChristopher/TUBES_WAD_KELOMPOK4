607340d6cc617ef8fcba60111d3b05f8dd46e58b
adityawahyunugraha <inyomanadityawahyunugraha@gmail.com>
2026-01-04 14:06:49 +0700

    Update kode pada model User

--- App/Models/User.php
+++ App/Models/User.php
@@ -49,9 +49,11 @@ class User extends Authenticatable
 
     public function skills()
     {
-        return $this->belongsToMany(Skill::class, 'user_skill')
-            ->withTimestamps();
+    return $this
+        ->belongsToMany(Skill::class, 'user_skill')
+        ->withTimestamps();
     }
+
 
     public function learningGoals()
     {
